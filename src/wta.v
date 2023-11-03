a3a591d3272052f32f17e26a6287fc40dd04fec9
Nick <nkuipers@ucsc.edu>
2023-11-02 18:58:23 -0700

    Got help from an adult on wta.v

--- src/wta.v
+++ src/wta.v
@@ -15,16 +15,18 @@ module wta (
             result <= 0;
         end else begin
             u_out <= result;
+            result[7:4] <= (comparator ? current[7:4] : 0);
+            result[3:0] <= (comparator ? 0 : current[3:0]);
         end
     end
 
     // if bits [7:4] are greater than bits [3:0] comparator returns 1, otherwise 0
     assign comparator = (current[7:4] >= current[3:0]);
 
-    // if comparator = 1, result[7:4] = current[7:4]
-    assign result[7:4] = (comparator ? current[7:4] : 0);
+    // // if comparator = 1, result[7:4] = current[7:4]
+    // assign result[7:4] = (comparator ? current[7:4] : 0);
 
-    // if comparator = 0, result [3:0] = current[3:0]
-    assign result[3:0] = (comparator ? 0 : current[3:0]);
+    // // if comparator = 0, result [3:0] = current[3:0]
+    // assign result[3:0] = (comparator ? 0 : current[3:0]);
 
 endmodule
